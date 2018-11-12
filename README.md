e469faa2adc2bfd6fee6442bc1ee084d48f2dd09
Jesus <japbogado@gmail.com>
2018-11-11 22:20:16 -0400

    Add a README and the fresh_install script

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dotfiles
+
+These are my dotfiles, a repo inspired by the need to migrate to another pc and the lazyness to install everything manually from scratch.
+
+Features my configs for:
+
+* Vim
+* Tmux
+* Ranger
+* Bash
+
+Also a script to symlink everything to `HOME` and another one to fresh install my setup.
+
+# Highlights
+
+* Some very useful aliases, look out for my [venv-utils]().
+* Very clean grey and blue vim and tmux themes which work perfectly together (Pics comming soon).
+* A vim inspired current dir bash prompt.
+
+# Credits
+
+I'll be adding credits to everything I steal from the internet soon.
---
+++ fresh_install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# This scripts supposedly replicates my setup on a new machine, it is not yet tested.
+
+#######################################
+
+# HELPER FUNCTIONS
+mkhomefolder() {
+    if [ ! -d $HOME/$1 ]; then
+        echo "Creating $1 folder."
+        mkdir $HOME/$1
+    fi
+}
+
+debinstall() {
+    echo "Downloading from $1/$2"
+    wget $1/$2
+    echo "Installing."
+    sudo gdebi $2
+    rm $2
+}
+
+# FOLDERS
+mkhomefolder .env_vars # Projects' environment variables
+mkhomefolder .scripts  # Projects' scripts
+mkhomefolder .venvs    # Projects' virtual environments
+
+# PROGRAMS
+sudo -v # Initial sudo to avoid asking for the password again
+
+sudo apt upgrade
+
+# Install basic programs
+sudo apt install git vim ranger htop tree tig redshift synaptic unrar r-base gdebi wget libevent-dev libncurses5-dev libxml2-dev libzip-dev
+
+# Install snaps
+sudo snap install mailspring telegram-desktop skype
+
+# Other instalations
+debinstall "https://download1.rstudio.org" "rstudio-xenial-1.1.463-amd64.deb"
+
+# Tmux
+git clone https://github.com/tmux/tmux.git
+wget https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
+cd tmux-2.8
+sh autogen.sh
+./configure && make
+sudo make install
+cd ..
+rm tmux-2.8
+
+# SC-IM
+git clone https://github.com/jmcnamara/libxlsxwriter.git
+cd libxlsxwriter/
+make
+sudo make install
+cd ..
+rm libxlsxwriter
+
+git clone https://github.com/andmarti1424/sc-im.git
+cd sc-im/src
+make
+sudo make install
+cd ../..
+rm sc-im
+
+#######################################
+
+# INSTALL DOTFILES
+echo "Downloading my dotfiles"
+git clone dotfiles ~/.dotfiles
+
+echo "Symlinking my dotfiles"
+. ~/.dotfiles/makesymlinks.sh
+
+# Prepare vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
+echo "DONE!"
