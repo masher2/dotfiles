de7e73515ca41c1aecd371c5e4714549d85b7b89
Jesus Perez <japbogado@gmail.com>
2018-11-16 00:00:53 -0400

    Add compton and feh to i3 installation

--- fresh_install.sh
+++ fresh_install.sh
@@ -34,7 +34,7 @@ sudo apt upgrade
 sudo apt install vim ranger htop tree tig redshift synaptic unrar r-base gdebi wget libevent-dev libncurses6-dev bison libncurses5-dev libncursesw5-dev libxml2-dev libzip-dev postgresql
 
 # I3-gaps dependencies
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake dmenu i3status xbacklight xserver-xorg-input-synaptics
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake dmenu i3status xbacklight xserver-xorg-input-synaptics compton feh
 
 # Installing i3
 git clone https://github.com/Airblader/i3.git
